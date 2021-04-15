9d0aa682d029ecb84454418784e2d737816e9384
fercomunello <nando.comunello@gmail.com>
2021-04-15 05:28:13 -0300

    Initial commit, only the project configuration.

---
+++ src/test/java/me/fernando/reactive/Main.java
@@ -0,0 +1,11 @@
+package me.fernando.reactive;
+
+import org.junit.jupiter.api.Test;
+
+public class Main {
+
+    @Test
+    public void test() {
+        System.out.println("Working!");
+    }
+}
---
+++ src/test/java/me/fernando/reactive/MonoTest.java
@@ -0,0 +1,15 @@
+package me.fernando.reactive;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MonoTest {
+
+    private static final Logger log = LoggerFactory.getLogger(MonoTest.class);
+
+    @Test
+    public void test() {
+        log.info("Working!");
+    }
+}
